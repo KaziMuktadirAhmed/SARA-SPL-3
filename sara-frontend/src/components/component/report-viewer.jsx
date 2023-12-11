99ceefb9eadd17528ae87ee4e6d706a5551f972c
KaziMuktadirAhmed <kazi.muktadir@brainstation-23.com>
2023-12-12 01:14:23 +0600

    add some ui

--- sara-frontend/src/components/component/report-viewer.jsx
+++ sara-frontend/src/components/component/report-viewer.jsx
@@ -1,44 +1,58 @@
+"use client";
+
+import { useState } from "react";
+
+import { CardTitle, CardHeader, CardContent, Card } from "@/components/ui/card";
 import { Button } from "@/components/ui/button";
 import { Tabs } from "@/components/ui/tabs";
-import { CardTitle, CardHeader, CardContent, Card } from "@/components/ui/card";
 import { Badge } from "@/components/ui/badge";
 import { Label } from "@/components/ui/label";
 
 export function ReportViewer() {
-  const saraTags = ["General", "Trust and Safety", "User Experience"];
+  const saraTags = ["All", "General", "Trust and Safety", "User Experience"];
+  const [selectedTab, setSelectedTab] = useState("All");
 
   const cards = [
     {
       title: "Browser errors were logged to the console",
-      badge: "General",
+      badge: ["General"],
       description:
         "Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
     },
     {
       title: "Missing source maps for large first-party JavaScript",
-      badge: "General",
+      badge: ["General"],
       description:
         "Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
     },
     {
       title: "Ensure CSP is effective against XSS attacks",
-      badge: "Trust and Safety",
+      badge: ["General", "Trust and Safety"],
       description:
         "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium.",
     },
     {
       title: "Issues were logged in the Issues panel in Chrome Devtools",
-      badge: "General",
+      badge: ["General"],
       description:
         "Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
     },
     {
       title: "Detected JavaScript libraries",
-      badge: "General",
+      badge: ["General", "User Experience"],
       description:
         "Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
     },
   ];
+
+  const handleTabClick = (tag) => {
+    setSelectedTab(tag);
+  };
+
+  const filteredCards =
+    selectedTab === "All"
+      ? cards
+      : cards.filter((card) => card.badge.includes(selectedTab));
 
   return (
     <div className="container p-6 mx-auto">
@@ -51,28 +65,34 @@ export function ReportViewer() {
           SARA tags:
         </Label>
         <div className="flex space-x-1">
-          <Button variant="ghost">All</Button>
           {saraTags.map((tag) => (
-            <Button key={tag} variant="ghost">
+            <Button
+              className={selectedTab === tag ? "border-black border-2" : ""}
+              key={tag}
+              variant={selectedTab === tag ? "outline" : "ghost"}
+              onClick={() => handleTabClick(tag)}
+            >
               {tag}
             </Button>
           ))}
         </div>
       </Tabs>
       <div className="space-y-4">
-        {cards.map((card, index) => (
+        {filteredCards.map((card, index) => (
           <Card key={index} className="w-full">
             <CardHeader>
               <CardTitle>{card.title}</CardTitle>
             </CardHeader>
             <CardContent>
-              <div className="flex">
-                <Badge className="mb-2" variant="outline">
-                  {card.badge}
-                </Badge>
+              <div className="flex gap-2">
+                {card.badge.map((badge, index) => (
+                  <Badge key={index} className="mb-2" variant="outline">
+                    {badge}
+                  </Badge>
+                ))}
               </div>
               <p className="text-gray-500">{card.description}</p>
-              <Button className="mt-4 text-sm" variant="ghost">
+              <Button className="mt-4 text-sm border-2" variant="ghost">
                 Expand
               </Button>
             </CardContent>

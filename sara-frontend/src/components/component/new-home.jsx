253e5960e0f143197c3bc40941da06c4b6ecb5b4
KaziMuktadirAhmed <kazi.muktadir@brainstation-23.com>
2023-12-11 22:43:38 +0600

    adds home page

---
+++ sara-frontend/src/components/component/new-home.jsx
@@ -0,0 +1,53 @@
+import { CardHeader, CardContent, Card } from "@/components/ui/card";
+import { Label } from "@/components/ui/label";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+
+export function NewHome() {
+  return (
+    <>
+      <div className="container flex flex-col items-center px-4 py-8 mx-auto">
+        <div className="mb-8 text-center">
+          <h1 className="mb-8 font-bold text-gray-800 text-8xl">SARA</h1>
+          <p className="text-xl text-gray-600">
+            A post processing tool for static analysis tools
+          </p>
+        </div>
+        <Card className="w-full max-w-md overflow-hidden bg-white rounded-lg shadow-md">
+          <CardHeader className="px-6 py-4">
+            <h2 className="text-xl font-bold text-gray-800">File Upload</h2>
+          </CardHeader>
+          <CardContent className="p-6">
+            <div className="grid gap-4">
+              <div className="grid w-full max-w-sm items-center gap-1.5">
+                <Label
+                  className="mb-2 text-sm font-bold text-gray-700"
+                  htmlFor="fileUpload"
+                >
+                  Upload File
+                </Label>
+                <Input id="fileUpload" type="file" />
+              </div>
+              <div className="grid w-full max-w-sm items-center gap-1.5">
+                <Label
+                  className="mb-2 text-sm font-bold text-gray-700"
+                  htmlFor="filePath"
+                >
+                  File Path
+                </Label>
+                <Input
+                  className="w-full px-3 py-2 leading-tight text-gray-700 border rounded shadow appearance-none focus:outline-none focus:shadow-outline"
+                  id="filePath"
+                  type="text"
+                />
+              </div>
+            </div>
+            <Button className="mt-6" size="lg" variant="outline">
+              Start
+            </Button>
+          </CardContent>
+        </Card>
+      </div>
+    </>
+  );
+}

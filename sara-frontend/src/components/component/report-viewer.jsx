a8af79581df8ab9448476b2b6da11010fc834bee
KaziMuktadirAhmed <kazi.muktadir@brainstation-23.com>
2023-12-12 00:30:22 +0600

    add some ui

--- sara-frontend/src/components/component/report-viewer.jsx
+++ sara-frontend/src/components/component/report-viewer.jsx
@@ -5,6 +5,41 @@ import { Badge } from "@/components/ui/badge";
 import { Label } from "@/components/ui/label";
 
 export function ReportViewer() {
+  const saraTags = ["General", "Trust and Safety", "User Experience"];
+
+  const cards = [
+    {
+      title: "Browser errors were logged to the console",
+      badge: "General",
+      description:
+        "Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+    },
+    {
+      title: "Missing source maps for large first-party JavaScript",
+      badge: "General",
+      description:
+        "Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+    },
+    {
+      title: "Ensure CSP is effective against XSS attacks",
+      badge: "Trust and Safety",
+      description:
+        "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium.",
+    },
+    {
+      title: "Issues were logged in the Issues panel in Chrome Devtools",
+      badge: "General",
+      description:
+        "Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+    },
+    {
+      title: "Detected JavaScript libraries",
+      badge: "General",
+      description:
+        "Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
+    },
+  ];
+
   return (
     <div className="container p-6 mx-auto">
       <h1 className="mb-6 text-3xl font-bold">Static analysis report</h1>
@@ -17,121 +52,32 @@ export function ReportViewer() {
         </Label>
         <div className="flex space-x-1">
           <Button variant="ghost">All</Button>
-          <Button variant="ghost">General</Button>
-          <Button variant="ghost">Trust and Safety</Button>
-          <Button variant="ghost">User Experience</Button>
+          {saraTags.map((tag) => (
+            <Button key={tag} variant="ghost">
+              {tag}
+            </Button>
+          ))}
         </div>
       </Tabs>
       <div className="space-y-4">
-        <Card className="w-full">
-          <CardHeader>
-            <CardTitle>Browser errors were logged to the console</CardTitle>
-          </CardHeader>
-          <CardContent>
-            <div className="flex">
-              {" "}
-              {/* Added flex class */}
-              <Badge className="mb-2" variant="outline">
-                General
-              </Badge>
-            </div>
-            <p className="text-gray-500">
-              Excepteur sint occaecat cupidatat non proident, sunt in culpa qui
-              officia deserunt mollit anim id est laborum.
-            </p>
-            <Button className="mt-4 text-sm" variant="ghost">
-              Expand
-            </Button>
-          </CardContent>
-        </Card>
-        <Card className="w-full">
-          <CardHeader>
-            <CardTitle>
-              Missing source maps for large first-party JavaScript
-            </CardTitle>
-          </CardHeader>
-          <CardContent>
-            <div className="flex">
-              {" "}
-              {/* Added flex class */}
-              <Badge className="mb-2" variant="outline">
-                General
-              </Badge>
-            </div>
-            <p className="text-gray-500">
-              Excepteur sint occaecat cupidatat non proident, sunt in culpa qui
-              officia deserunt mollit anim id est laborum.
-            </p>
-            <Button className="mt-4 text-sm" variant="ghost">
-              Expand
-            </Button>
-          </CardContent>
-        </Card>
-        <Card className="w-full">
-          <CardHeader>
-            <CardTitle>Ensure CSP is effective against XSS attacks</CardTitle>
-          </CardHeader>
-          <CardContent>
-            <div className="flex">
-              {" "}
-              {/* Added flex class */}
-              <Badge className="mb-2" variant="outline">
-                Trust and Safety
-              </Badge>
-            </div>
-            <p className="text-gray-500">
-              Sed ut perspiciatis unde omnis iste natus error sit voluptatem
-              accusantium doloremque laudantium.
-            </p>
-            <Button className="mt-4 text-sm" variant="ghost">
-              Expand
-            </Button>
-          </CardContent>
-        </Card>
-        <Card className="w-full">
-          <CardHeader>
-            <CardTitle>
-              Issues were logged in the Issues panel in Chrome Devtools
-            </CardTitle>
-          </CardHeader>
-          <CardContent>
-            <div className="flex">
-              {" "}
-              {/* Added flex class */}
-              <Badge className="mb-2" variant="outline">
-                General
-              </Badge>
-            </div>
-            <p className="text-gray-500">
-              Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris
-              nisi ut aliquip ex ea commodo consequat.
-            </p>
-            <Button className="mt-4 text-sm" variant="ghost">
-              Expand
-            </Button>
-          </CardContent>
-        </Card>
-        <Card className="w-full">
-          <CardHeader>
-            <CardTitle>Detected JavaScript libraries</CardTitle>
-          </CardHeader>
-          <CardContent>
-            <div className="flex">
-              {" "}
-              {/* Added flex class */}
-              <Badge className="mb-2" variant="outline">
-                General
-              </Badge>
-            </div>
-            <p className="text-gray-500">
-              Duis aute irure dolor in reprehenderit in voluptate velit esse
-              cillum dolore eu fugiat nulla pariatur.
-            </p>
-            <Button className="mt-4 text-sm" variant="ghost">
-              Expand
-            </Button>
-          </CardContent>
-        </Card>
+        {cards.map((card, index) => (
+          <Card key={index} className="w-full">
+            <CardHeader>
+              <CardTitle>{card.title}</CardTitle>
+            </CardHeader>
+            <CardContent>
+              <div className="flex">
+                <Badge className="mb-2" variant="outline">
+                  {card.badge}
+                </Badge>
+              </div>
+              <p className="text-gray-500">{card.description}</p>
+              <Button className="mt-4 text-sm" variant="ghost">
+                Expand
+              </Button>
+            </CardContent>
+          </Card>
+        ))}
       </div>
     </div>
   );
